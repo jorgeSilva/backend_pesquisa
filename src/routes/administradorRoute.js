67056f659af325d8403d75d1c8248f2a07d831fb
Jorge Silva <jorgelfsilva5@gmail.com>
2023-07-27 12:09:55 -0300

    adição do middleware na rota get '/adm/:_id' para buscar os dados do administrador

--- src/routes/administradorRoute.js
+++ src/routes/administradorRoute.js
@@ -2,8 +2,10 @@ const express = require('express')
 const admRoute = express.Router()
 
 const AdministradorController = require('../controller/administradorController')
+const UserMiddleware = require('../middleware/UserMiddleware')
 
 admRoute.post('/adm', AdministradorController.store)
 admRoute.post('/adm/login', AdministradorController.login)
+admRoute.get('/adm/:_id', UserMiddleware, AdministradorController.index)
 
 module.exports = admRoute
