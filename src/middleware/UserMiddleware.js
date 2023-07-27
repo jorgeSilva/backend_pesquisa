4be0f0949ec8db51e421ceec91480c9f371bb78c
Jorge Silva <jorgelfsilva5@gmail.com>
2023-07-27 11:33:48 -0300

    criação de um middleware para proteção da rota ao ver os dados dos clientes

---
+++ src/middleware/UserMiddleware.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const CandidatoModel = require('../model/candidato')
+
+const UserMiddleware = (req, res, next) => {
+  const authUser = req.headers['authorization']
+  const token = authUser && authUser.split(' ')[1]
+
+  if(!token){
+    return res.status(401).json({error: 'Acesso negado'})
+  }
+
+  try{
+    const secret = process.env.SECRET
+    jwt.verify(token, secret)
+
+    next()
+  }catch(error){
+    res.status(400).json({error: 'Tokén invalido.'})
+  }
+}
